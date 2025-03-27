d009c622f820e3efe587d6259879c73647ab96b0
kenji-yt <kenjiyiptong@gmail.com>
2025-03-27 16:01:13 +0100

    latest

--- workflow/scripts/make_reproducibility_report.sh
+++ workflow/scripts/make_reproducibility_report.sh
@@ -4,8 +4,8 @@
 
 input_dir=$1
 n_cores=$2
-snake_genespace_report=results/snake_GENESPACE_reproducibility_report.txt
 snake_eagle_rc_report=results/snake_EAGLE_RC_reproducibility_report.txt
+snake_genespace_report=results/snake_GENESPACE_reproducibility_report.txt
 report=results/HEAL_reproducibility_report.txt
 CURRENT_DATETIME=$(date +"%Y-%m-%d %H:%M:%S")
 
@@ -20,6 +20,34 @@ echo "Number of allocated cores: ${n_cores}" >> "${report}"
 echo "" >> "${report}"
 echo "" >> "${report}"
 
-
 version_HEAL=$(git describe --tags --abbrev=0 | sed 's/v//g')
 echo "HEAL=${version_HEAL}" >> "${report}"
+
+awk '/\* TOOLS \*/ {flag=1; next} /\* OUTPUT FILES \*/ {flag=0} flag' $snake_eagle_rc_report \
+ | grep -v '^$' | grep -v '^*' >> "${report}"
+
+awk '/\* TOOLS \*/ {flag=1; next} /\* OUTPUT FILES \*/ {flag=0} flag' $snake_genespace_report \
+ | grep -v '^$' | grep -v '^*' >> "${report}"
+
+echo "" >> "${report}"
+echo "" >> "${report}"
+echo "" >> "${report}"
+echo "****************" >> "${report}"
+echo "* OUTPUT FILES *" >> "${report}"
+echo "****************" >> "${report}"
+echo "" >> "${report}"
+echo "" >> "${report}"
+
+if [ "$OS" == "Linux" ]; then
+    echo "Linux md5sum checksums for the output files" >> "${report}"
+    find results/healr/stats/ -type f | xargs -n${n_cores} md5sum | awk '{print $2"\t"$1}' >> "${report}"  
+else
+    echo "Mac md5 checksums for the input files" >> "${report}"
+    find results/healr/stats/ -type f | xargs -n${n_cores} md5 | awk '{print $2"\t"$4}' >> "${report}"  
+fi
+echo "" >> "${report}"
+awk '/\* OUTPUT FILES \*/ {flag=1; next} flag' $snake_eagle_rc_report \
+| grep -v '^$' | grep -v '^*' | grep -v '^Mac md5' | grep -v '^Linux md5sum' >> "${report}"
+echo "" >> "${report}"
+awk '/\* OUTPUT FILES \*/ {flag=1; next} flag' $snake_genespace_report \
+| grep -v '^$' | grep -v '^*' | grep -v '^Mac md5' | grep -v '^Linux md5sum' >> "${report}"
